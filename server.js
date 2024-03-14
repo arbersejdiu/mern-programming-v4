a6dd24f8e5f4b200d0fa674e8ac01dec64d8bb42
Arber Sejdiu <arbersejdiu.upwork@gmail.com>
2024-03-14 20:19:11 +0100

    Remove body-parser

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const app = express();
 require("dotenv").config;
 require("./config/db");
 const port = process.env.PORT || 3000;
-
 const Routes = require("./routes/studentsRoute");
+
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static("public"));
 
 app.use(Routes);
